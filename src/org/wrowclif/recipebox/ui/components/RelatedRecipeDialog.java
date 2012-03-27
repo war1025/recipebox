13f63daaace60a542345eb3f383a70ed496070fe
Wayne Rowcliffe <war1025@gmail.com>
2012-03-26 22:14:40 -0500

    Add RelatedRecipe widget and dialog

---
+++ src/org/wrowclif/recipebox/ui/components/RelatedRecipeDialog.java
@@ -0,0 +1,244 @@
+package org.wrowclif.recipebox.ui.components;
+
+import org.wrowclif.recipebox.Recipe;
+import org.wrowclif.recipebox.R;
+
+import org.wrowclif.recipebox.impl.UtilityImpl;
+
+import org.wrowclif.recipebox.ui.components.ListAutoCompleteAdapter;
+import org.wrowclif.recipebox.ui.components.ListAutoCompleteAdapter.Specifics;
+import org.wrowclif.recipebox.ui.components.DynamicLoadAdapter;
+
+import static org.wrowclif.recipebox.util.ConstantInitializer.assignId;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+public class RelatedRecipeDialog extends Dialog {
+
+	private AutoCompleteTextView recipeInput;
+	private TextView messageView;
+	private Button okButton;
+	private Button cancelButton;
+
+	private Recipe recipe;
+	private RelatedRecipeListWidget adapter;
+	private Recipe suggestion;
+
+	private View.OnClickListener newOkOnClick;
+	private View.OnClickListener newCancelOnClick;
+	private OnEditorActionListener newOnRecipeAction;
+	private View.OnClickListener deleteOkOnClick;
+	private View.OnClickListener deleteCancelOnClick;
+	private View.OnClickListener invalidCancelOnClick;
+
+	public RelatedRecipeDialog(Context context, Recipe recipe, RelatedRecipeListWidget adapter) {
+		super(context);
+		setContentView(R.layout.category_add_dialog);
+
+		this.recipe = recipe;
+		this.adapter = adapter;
+		this.suggestion = null;
+
+		getWindow().setLayout(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+		setCancelable(true);
+
+		recipeInput = (AutoCompleteTextView) findViewById(R.id.category_name);
+		messageView = (TextView) findViewById(R.id.message_box);
+		okButton = (Button) findViewById(R.id.ok_button);
+		cancelButton = (Button) findViewById(R.id.cancel_button);
+
+		connectAutoComplete();
+
+		recipeInput.setHint("Recipe");
+
+		newOkOnClick = new NewOkOnClick();
+		newCancelOnClick = new CancelOnClick();
+		newOnRecipeAction = new NewOnRecipeAction();
+
+		deleteOkOnClick = new DeleteOkOnClick();
+		deleteCancelOnClick = newCancelOnClick;
+
+		invalidCancelOnClick = new CancelReturnToEditOnClick();
+	}
+
+	public void prepareNew() {
+		showNew();
+	}
+
+	public void prepareDelete(Recipe suggestion) {
+		this.suggestion = suggestion;
+
+		showDelete();
+	}
+
+	public void showDelete() {
+		setTitle("Remove Relation");
+
+		recipeInput.setVisibility(View.GONE);
+		okButton.setVisibility(View.VISIBLE);
+		cancelButton.setVisibility(View.VISIBLE);
+		messageView.setVisibility(View.VISIBLE);
+
+		messageView.setText("Are you sure you want to remove the relation between " + recipe.getName() +
+							" and " + suggestion.getName() + "?");
+
+		okButton.setText("Remove");
+		okButton.setOnClickListener(deleteOkOnClick);
+
+		cancelButton.setText("Cancel");
+		cancelButton.setOnClickListener(deleteCancelOnClick);
+	}
+
+	public void showNew() {
+		reshowNew();
+
+		recipeInput.setText("");
+	}
+
+	public void reshowNew() {
+		setTitle("Add Relation");
+
+		recipeInput.setVisibility(View.VISIBLE);
+		okButton.setVisibility(View.VISIBLE);
+		cancelButton.setVisibility(View.VISIBLE);
+		messageView.setVisibility(View.GONE);
+
+		recipeInput.setOnEditorActionListener(newOnRecipeAction);
+
+		okButton.setText("Add");
+		okButton.setOnClickListener(newOkOnClick);
+
+		cancelButton.setText("Cancel");
+		cancelButton.setOnClickListener(newCancelOnClick);
+	}
+
+	public void showInvalidRelation() {
+		setTitle("Cannot Add Relation");
+
+		recipeInput.setVisibility(View.GONE);
+		okButton.setVisibility(View.GONE);
+		cancelButton.setVisibility(View.VISIBLE);
+		messageView.setVisibility(View.VISIBLE);
+
+		if(suggestion == null) {
+			messageView.setText("No recipes named " + recipeInput.getText() + " exist");
+		} else {
+			messageView.setText("A recipe cannot be related to itself");
+		}
+
+		cancelButton.setText("Ok");
+		cancelButton.setOnClickListener(invalidCancelOnClick);
+	}
+
+	private void connectAutoComplete() {
+		Specifics<Recipe> sp = new Specifics<Recipe>() {
+
+			public View getView(int id, Recipe r, View v, ViewGroup vg) {
+				if(v == null) {
+					v = inflate(R.layout.autoitem);
+				}
+
+				TextView tv = (TextView) v.findViewById(R.id.child_name);
+
+				tv.setText(r.getName());
+
+				return v;
+			}
+
+			public long getItemId(Recipe item) {
+				return item.getId();
+			}
+
+			public List<Recipe> filter(CharSequence seq) {
+				return UtilityImpl.singleton.searchRecipes(seq.toString(), 5);
+			}
+
+			public String convertResultToString(Recipe result) {
+				return result.getName();
+			}
+
+			public void onItemClick(AdapterView av, View v, int position, long id, Recipe item) {
+
+			}
+
+			private View inflate(int layoutId) {
+				LayoutInflater vi = getLayoutInflater();
+				return vi.inflate(layoutId, null);
+			}
+		};
+
+		ListAutoCompleteAdapter<Recipe> acadapter = new ListAutoCompleteAdapter<Recipe>(sp);
+
+		recipeInput.setAdapter(acadapter);
+	}
+
+	protected class NewOkOnClick implements View.OnClickListener {
+		public void onClick(View v) {
+			List<Recipe> r = UtilityImpl.singleton.searchRecipes(recipeInput.getText().toString(), 1);
+
+			suggestion = (r.size() > 0) ? r.get(0) : null;
+
+			if(suggestion == null) {
+				showInvalidRelation();
+			} else if(suggestion.getId() == recipe.getId()) {
+				showInvalidRelation();
+			} else {
+				recipe.addSuggestion(suggestion);
+
+				adapter.refresh();
+				RelatedRecipeDialog.this.dismiss();
+			}
+		}
+	}
+
+	protected class NewOnRecipeAction implements OnEditorActionListener {
+		public boolean onEditorAction(TextView v, int action, KeyEvent event) {
+			if((action == EditorInfo.IME_NULL) && (event.getAction() == 0)) {
+				newOkOnClick.onClick(null);
+				return true;
+			}
+			return false;
+		}
+	}
+
+	protected class CancelOnClick implements View.OnClickListener {
+		public void onClick(View v) {
+			RelatedRecipeDialog.this.dismiss();
+		}
+	}
+
+	protected class DeleteOkOnClick implements View.OnClickListener {
+		public void onClick(View v) {
+			recipe.removeSuggestion(suggestion);
+
+			adapter.refresh();
+			RelatedRecipeDialog.this.dismiss();
+		}
+	}
+
+	protected class CancelReturnToEditOnClick implements View.OnClickListener {
+		public void onClick(View v) {
+			reshowNew();
+		}
+	}
+}
---
+++ src/org/wrowclif/recipebox/ui/components/RelatedRecipeListWidget.java
@@ -0,0 +1,152 @@
+package org.wrowclif.recipebox.ui.components;
+
+import org.wrowclif.recipebox.R;
+
+import org.wrowclif.recipebox.Recipe;
+import org.wrowclif.recipebox.Suggestion;
+import org.wrowclif.recipebox.Utility;
+
+import org.wrowclif.recipebox.impl.UtilityImpl;
+
+import org.wrowclif.recipebox.ui.RecipeTabs;
+
+import static org.wrowclif.recipebox.util.ConstantInitializer.assignId;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RelatedRecipeListWidget {
+
+	private static final int REMOVE_RELATED_RECIPE_DIALOG = assignId();
+	private static final int ADD_RELATED_RECIPE_DIALOG = assignId();
+
+	private Recipe recipe;
+	private Utility util;
+	private Activity context;
+	private View listWidget;
+	private Button addButton;
+	private ViewGroup recipes;
+	private RelatedRecipeDialog relatedDialog;
+	private boolean editing;
+
+	public RelatedRecipeListWidget(Recipe recipe, Activity context) {
+		this.recipe = recipe;
+		this.context = context;
+		this.listWidget = getLayoutInflater().inflate(R.layout.category_list_widget, null);
+		this.recipes = (ViewGroup) listWidget.findViewById(R.id.category_list);
+		this.editing = false;
+		this.util = UtilityImpl.singleton;
+
+		TextView title = (TextView) listWidget.findViewById(R.id.category_label);
+		title.setText("Related Recipes");
+
+		this.addButton = (Button) listWidget.findViewById(R.id.category_button);
+		addButton.setText("Add Recipe");
+
+		addButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				RelatedRecipeListWidget.this.context.showDialog(ADD_RELATED_RECIPE_DIALOG);
+			}
+		});
+	}
+
+	public View getView() {
+		refresh();
+		return listWidget;
+	}
+
+	public void refresh() {
+		recipes.removeAllViews();
+		for(Suggestion s : recipe.getSuggestedWith()) {
+			recipes.addView(createRecipeEntry(s));
+		}
+		recipes.invalidate();
+	}
+
+	protected View createRecipeEntry(Suggestion s) {
+
+		final Recipe suggested = s.getSuggestedRecipe();
+		final long id = suggested.getId();
+
+		View v = getLayoutInflater().inflate(R.layout.category_item, null);
+		TextView ctv = (TextView) v.findViewById(R.id.name_box);
+		ctv.setText(suggested.getName());
+
+		TextView idBox = (TextView) v.findViewById(R.id.edit_button);
+		idBox.setText(id + "");
+
+		v.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent i = new Intent(context, RecipeTabs.class);
+				i.putExtra("id", id);
+				context.startActivity(i);
+			}
+		});
+
+		View deleteButton = v.findViewById(R.id.delete_button);
+		deleteButton.setVisibility((editing) ? View.VISIBLE : View.GONE);
+
+		deleteButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Bundle bundle = new Bundle();
+
+				bundle.putLong("id", id);
+				context.showDialog(REMOVE_RELATED_RECIPE_DIALOG, bundle);
+			}
+		});
+
+		return v;
+	}
+
+	public void setEditing(boolean editing) {
+		this.editing = editing;
+		if(editing) {
+			listWidget.setVisibility(View.VISIBLE);
+		} else if(recipes.getChildCount() == 0) {
+			listWidget.setVisibility(View.GONE);
+		}
+		for(int i = 0; i < recipes.getChildCount(); i++) {
+			View child = recipes.getChildAt(i);
+			View deleteButton = child.findViewById(R.id.delete_button);
+			deleteButton.setVisibility((editing) ? View.VISIBLE : View.GONE);
+		}
+		addButton.setVisibility((editing) ? View.VISIBLE : View.GONE);
+	}
+
+	public Dialog createDialog(int id) {
+		Dialog retDialog = null;
+		if((id == ADD_RELATED_RECIPE_DIALOG) || (id == REMOVE_RELATED_RECIPE_DIALOG)) {
+			if(relatedDialog == null) {
+				relatedDialog = new RelatedRecipeDialog(context, recipe, this);
+			}
+			retDialog = relatedDialog;
+		}
+		return retDialog;
+	}
+
+	public boolean prepareDialog(int id, Dialog dialog, Bundle bundle) {
+		boolean handled = false;
+		if(id == ADD_RELATED_RECIPE_DIALOG) {
+			((RelatedRecipeDialog) dialog).prepareNew();
+			handled = true;
+		} else if(id == REMOVE_RELATED_RECIPE_DIALOG) {
+			long rid = bundle.getLong("id", -1);
+			((RelatedRecipeDialog) dialog).prepareDelete(util.getRecipeById(rid));
+			handled = true;
+		}
+		return handled;
+	}
+
+	protected LayoutInflater getLayoutInflater() {
+		return (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+}
